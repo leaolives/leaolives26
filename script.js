76a2b42eef0aecf0a7cadd66b65097d330793838
leaolives <leaolivesoficial@gmail.com>
2026-04-27 16:16:37 -0300

    atualização jogos quarta

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // --- 1. CONFIGURAÇÃO DOS JOGOS ---
 const matches = {
   fortaleza: {
-    title: "Operario x Fortaleza",
-    meta: "26/04 • 18:00",
+    title: "Fortaleza x Sport",
+    meta: "29/04 • 21:30",
     players: [
-      { type: "hls", url: "https://xn---22--11--33--88--75---------b25zjfpkmbt1n9g9zza94e.xn----------------g34l3fkcn6n2hmd3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e7qri.xn--1ck2e1b/docs/redetv/__index.m3u8?cc=y&sv=196&nu3zAQc9HC3GbwJq=1777215821-qgI1NHdRyMWxeMStU1PNiJ86FqJkJeMkSi4MWeF6jUs%3D" },
+      { type: "iframe", url: "https://www.youtube.com/embed/PoXrobHfUmI?si=v9oTkhu-B6BH504I" },
       { type: "hls", url: "https://dlnmh9ip6v2xc9xx.cloudfontes.net/redetv.m3u8" },
       { type: "iframe", url: "https://esportesembed.com/operario-ferroviario-x-fortaleza-1" }
     ]
   },
   ceara: {
-    title: "Ceará x Vila Nova",
-    meta: "26/04 • 18:00",
+    title: "Maranhão x Ceará",
+    meta: "29/04 • 21:30",
     players: [
       { type: "hls", url: "https://xn---22--11--33--88--75---------b25zjfpkmbt1n9g9zza94e.xn----------------g34l3fkcn6n2hmd3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e7qri.xn--1ck2e1b/docs/xsports/__index.m3u8?cc=y&sv=38&nu3zAQc9HC3GbwJq=1777215946-%2BUmWqOy7I3%2BYIBZanyoVDcjzSyawF6xDa8xQntKlq6A%3D" },
       { type: "hls", url: "https://dlnmh9ip6v2xc9xx.cloudfontes.net/espn.m3u8" },
