d99362ae0530d806a5e93122c84b5f4c6a1166ca
leaolives <leaolivesoficial@gmail.com>
2026-05-03 10:15:50 -0300

    jogo fortaleza e links ceara

--- script.js
+++ script.js
@@ -29,12 +29,12 @@ const matches = {
       {
         type: "hls",
         engine: "clappr",
-        url: "https://xn---22--11--33--88--75---------b25zjfpkmbt1n9g9zza94e.xn----------------g34l3fkcn6n2hmd3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e7qri.xn--1ck2e1b/docs/espn/__index.m3u8?cc=y&sv=176&nu3zAQc9HC3GbwJq=1777741344-h1Cz4SXDLeIYxB%2FD7dRNgWiLk5UHANdWAqS4PTSOKDg%3D"
+        url: "https://xn---22--11--33--88--75---------b25zjfpkmbt1n9g9zza94e.xn----------------g34l3fkcn6n2hmd3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e7qri.xn--1ck2e1b/docs/espn/__index.m3u8?cc=y&sv=82&nu3zAQc9HC3GbwJq=1777814056-%2BfsDIFDjLIHO%2F9kmB6RBb8K5t1OcZrpk1cvECmhB7eE%3D"
       },
       {
         type: "hls",
         engine: "dplayer",
-        url: "https://dlnmh9ip6v2xc9xx.cloudfontes.net/espn.m3u8"
+        url: "https://xn---22--11--33--88--75---------b25zjfpkmbt1n9g9zza94e.xn----------------g34l3fkcn6n2hmd3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e7qri.xn--1ck2e1b/docs/xsports/__index.m3u8?cc=y&sv=166&nu3zAQc9HC3GbwJq=1777814101-fGjp%2FXfkvolaUtDEXEl7g8ZpztgHs%2Bo3LMCRgGTaNgQ%3D"
       },
       {
         type: "hls",
