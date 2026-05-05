9e0f60f15a943aa4eccb0c65749d59ccb43e0fa1
leaolives <leaolivesoficial@gmail.com>
2026-05-05 19:57:49 -0300

    links ceara

--- script.js
+++ script.js
@@ -27,18 +27,18 @@ const matches = {
     meta: "06/05 • 21:30",
     players: [
       {
-        type: "hls",
+        type: "iframe",
         engine: "clappr",
-        url: "https://xn---22--11--33--88--75---------b25zjfpkmbt1n9g9zza94e.xn----------------g34l3fkcn6n2hmd3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e7qri.xn--1ck2e1b/docs/espn/__index.m3u8?cc=y&sv=82&nu3zAQc9HC3GbwJq=1777814056-%2BfsDIFDjLIHO%2F9kmB6RBb8K5t1OcZrpk1cvECmhB7eE%3D"
+        url: "https://www.youtube.com/embed/mBY8EcO4vhM?si=8jSDTtt8lguERwxy"
       },
       {
-        type: "hls",
+        type: "iframe",
         engine: "dplayer",
-        url: "https://xn---22--11--33--88--75---------b25zjfpkmbt1n9g9zza94e.xn----------------g34l3fkcn6n2hmd3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e7qri.xn--1ck2e1b/docs/xsports/__index.m3u8?cc=y&sv=166&nu3zAQc9HC3GbwJq=1777814101-fGjp%2FXfkvolaUtDEXEl7g8ZpztgHs%2Bo3LMCRgGTaNgQ%3D"
+        url: "https://www.youtube.com/embed/mBY8EcO4vhM?si=8jSDTtt8lguERwxy"
       },
       {
-        type: "hls",
-        url: "https://stream2.playercdn.xyz/stream/ozoo2/ed6dbc25462da493c8ec0695ddf26344/espn.txt"
+        type: "iframe",
+        url: "https://www.youtube.com/embed/mBY8EcO4vhM?si=8jSDTtt8lguERwxy"
       }
     ]
   }
