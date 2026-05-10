3de08061409dddf2067e816480fd40fce9c9fb76
leaolives <leaolivesoficial@gmail.com>
2026-05-10 18:26:13 -0300

    Change stream type from iframe to hls and vice versa

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const matches = {
     meta: "10/05 • 18:30",
     players: [
       {
-        type: "iframe",
+        type: "hls",
         engine: "clappr",
-        url: "https://5embeddecanais.xyz/disneyplus/"
+        url: "https://xn---22--11--33--88--75---------b25zjfpkmbt1n9g9zza94e.xn----------------g34l3fkcn6n2hmd3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e7qri.xn--1ck2e1b/docs/disneyplus1/__index.m3u8?cc=y&sv=208&nu3zAQc9HC3GbwJq=1778422765-EPGkuFSb5pV6wDGXeLbyZyjIYJ7iPBwW%2BLpEsF9SlW4%3D"
       },
       {
         type: "iframe",
@@ -15,9 +15,9 @@ const matches = {
         url: "https://esportesembed.com/avai-x-fortaleza-1"
       },
       {
-        type: "hls",
+        type: "iframe",
         engine: "clappr",
-        url: "https://xn---22--11--33--88--75---------b25zjfpkmbt1n9g9zza94e.xn----------------g34l3fkcn6n2hmd3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e7qri.xn--1ck2e1b/docs/disneyplus1/__index.m3u8?cc=y&sv=208&nu3zAQc9HC3GbwJq=1778422765-EPGkuFSb5pV6wDGXeLbyZyjIYJ7iPBwW%2BLpEsF9SlW4%3D"
+        url: "https://5embeddecanais.xyz/disneyplus/"
       }
     ]
   },
