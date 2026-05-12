fefeb1316403a3a9d63dff9a206e61495da89d37
leaolives <leaolivesoficial@gmail.com>
2026-05-12 20:46:43 -0300

    links ceara

--- script.js
+++ script.js
@@ -29,16 +29,16 @@ const matches = {
       {
         type: "hls",
         engine: "clappr",
-        url: "https://xn---22--11--33--88--75---------b25zjfpkmbt1n9g9zza94e.xn----------------g34l3fkcn6n2hmd3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e7qri.xn--1ck2e1b/docs/espn/__index.m3u8?cc=y&sv=4&nu3zAQc9HC3GbwJq=1778250958-TY%2B%2FwtAK4K5XaKs9jIO8aRpboaXPWKYyBeO3zEMBNeE%3D"
+        url: "https://xn---22--11--33--88--75---------b25zjfpkmbt1n9g9zza94e.xn----------------g34l3fkcn6n2hmd3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e7qri.xn--1ck2e1b/docs/sportv1/__index.m3u8?cc=y&sv=187&nu3zAQc9HC3GbwJq=1778629346-Qn%2BrNPFSF3szzAazximaZX%2Bmu5F%2Fo4J0Sj%2B%2BhPnQ3No%3D"
       },
       {
         type: "hls",
         engine: "clappr",
-        url: "https://xn---22--11--33--88--75---------b25zjfpkmbt1n9g9zza94e.xn----------------g34l3fkcn6n2hmd3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e7qri.xn--1ck2e1b/docs/xsports/__index.m3u8?cc=y&sv=192&nu3zAQc9HC3GbwJq=1778251082-cgaEFJwCJLsxHydqthwHEgN%2FpvbSRKlExM2pmdpF3yI%3D"
+        url: "https://d1muf25xa05so8hp12v.cloudflaire.lat/assets/themes/ozoo_dcc41e174eb83f535c9f3fc8aeae151a/style.css"
       },
       {
         type: "iframe",
-        url: "https://5embeddecanais.xyz/espn/"
+        url: "https://5embeddecanais.xyz/sportv/"
       }
     ]
   }
