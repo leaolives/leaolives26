a1cae6329e22aa86f691f4c6f3e70a5afbf6cfe3
leaolives <leaolivesoficial@gmail.com>
2026-05-14 19:46:47 -0300

    linksdecer

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ const matches = {
       {
         type: "hls",
         engine: "clappr",
-        url: "https://xn---22--11--33--88--75---------b25zjfpkmbt1n9g9zza94e.xn----------------g34l3fkcn6n2hmd3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e7qri.xn--1ck2e1b/docs/primevideo3/__index.m3u8?cc=y&sv=173&nu3zAQc9HC3GbwJq=1778770325-EISzSdIs3n2NwddO1KwQKdPQ9yRFwSn1csS2FSdvcZo%3D"
+        url: "https://5d3f545a14f5660de89f13890881daa4.cloudflaire.lat/assets/themes/embedtv_09844088e71cc2873d82f1fd8e24463d/style.css"
       },
       {
         type: "iframe",
         engine: "clappr",
-        url: "https://6embeddecanais.xyz/amazonprimevideo03/"
+        url: "https://v4.sportssonline.click/channels/bra/br4.php"
       },
       {
         type: "iframe",
         engine: "clappr",
-        url: "https://v3.sportssonline.click/channels/bra/br4.php"
+        url: "https://esportesembed.com/crb-x-fortaleza-1"
       }
     ]
   },
