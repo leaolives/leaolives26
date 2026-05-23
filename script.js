a133501ec023fccf4d53c2d98701569a931e4b52
leaolives <leaolivesoficial@gmail.com>
2026-05-23 18:27:42 -0300

    links fecc

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const matches = {
     meta: "23/05 • 18:30",
     players: [
       {
-        type: "hls",
+        type: "iframe",
         engine: "clappr",
-        url: "https://xn---22--11--33--88--75---------b25zjfpkmbt1n9g9zza94e.xn----------------g34l3fkcn6n2hmd3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e7qri.xn--1ck2e1b/docs/espn/__index.m3u8?cc=y&sv=197&nu3zAQc9HC3GbwJq=1779549945-bVT%2FwARkBQyvgDZezqm0xu0NtM6ZjQ6fUT6jqHVZoGs%3D"
+        url: "https://esportesembed.com/fortaleza-x-londrina-1"
       },
       {
         type: "hls",
