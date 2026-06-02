2807a12e0d4d82bab6823d6adb847d80963e7e57
leaolives <leaolivesoficial@gmail.com>
2026-06-02 16:24:46 -0300

    links fec x vit

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ const matches = {
       {
         type: "iframe",
         engine: "clappr",
-        url: "https://esportesembed.com/athletic-x-fortaleza-1"
+        url: "https://www.youtube.com/embed/0K8yTYd5DfI?si=KX3vmOXw_awS6Jui"
       },
       {
         type: "iframe",
         engine: "clappr",
-        url: "https://8embeddecanais.xyz/disneyplus/"
+        url: "https://www.youtube.com/embed/YyXZwPE6SwE?si=0Wq6epoN_0iFY4Az"
       },
       {
-        type: "hls",
+        type: "iframe",
         engine: "clappr",
-        url: "https://xn---22--11--33--88--75---------b25zjfpkmbt1n9g9zza94e.xn----------------g34l3fkcn6n2hmd3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e7qri.xn--1ck2e1b/docs/disneyplus1/__index.m3u8?cc=y&sv=40&nu3zAQc9HC3GbwJq=1780142363-g57P6T6awOCj1P5hlTNGY8zG1Tmf5J0JwNFqDxBIojg%3D"
+        url: "https://www.youtube.com/embed/0K8yTYd5DfI?si=KX3vmOXw_awS6Jui"
       }
     ]
   },
